bb1f62ee8f8e8463d9c3072a3cf28ea552739faf
Mamykin Sergei <mamykin.sergei@gmail.com>
2019-12-04 19:07:45 +0300

    Controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        $users = ['adam', 'Robert' ];
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'users' => $users,
+        ]);
+    }
+}
