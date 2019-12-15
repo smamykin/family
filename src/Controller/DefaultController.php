86f5174eca63064b19be116cbd0e34466b5bb502
Mamykin Sergei <mamykin.sergei@gmail.com>
2019-12-15 20:58:08 +0300

    некоторые операции crud

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -74,5 +74,45 @@ class DefaultController extends AbstractController
     public function mostPopularPosts($param)
     {
         return new Response('some new data' . $param);
+    }
+
+    /**
+     * @Route("create-user", name="create-user")
+     */
+    public function createUser()
+    {
+        $rep = $this->getDoctrine()->getRepository(User::class);
+        $user = $rep->find(6);
+        dump($user);
+        $user = $rep->findOneBy(['name'=> 'name - 1']);
+        dump($user);
+
+
+
+        return $this->render('default/crud.html.twig');
+    }
+
+    /**
+     * @Route("/update-if-exists/{param?}", name="crud-update")
+     * @param $param
+     * @return Response
+     */
+    public function updateUser($param)
+    {
+        $rep = $this->getDoctrine()->getRepository(User::class);
+        $id = $param ?? 8 ;
+        /** @var User $user */
+        $user = $rep->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException('No user for id ' . $id);
+        }
+
+        $user->setName('New name');
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        dump($user);
+
+        return $this->render('default/crud.html.twig');
     }
 }
