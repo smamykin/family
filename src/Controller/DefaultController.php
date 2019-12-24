8d99fe35bfdefef57f4d684bbabc5f50159288e3
Mamykin Sergei <mamykin.sergei@gmail.com>
2019-12-24 17:41:32 +0300

    Баловался с переопределением шаблонов бандла

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,26 +3,68 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Services\GiftsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends AbstractController
 {
     /**
+     * @var array
+     */
+    private $gifts;
+
+    public function __construct(GiftsService $gifts, $logger)
+    {
+        $this->gifts = $gifts->gifts;
+        $this->logger = $logger;
+    }
+
+    /**
      * @Route("/", name="index")
+     * @return Response
      */
     public function index()
     {
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
 
+        if ($users) {
+            throw $this->createNotFoundException('hi it\'s exception');
+        }
 
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-            'users' => $users,
-        ]);
+        return $this->render(
+            'default/index.html.twig',
+            [
+                'controller_name' => 'DefaultController',
+                'users' => $users,
+                'random_gift' => $this->gifts
+            ]
+        );
+    }
+
+    /**
+     * @Route(
+     *     "/articles/{_locale}/{year}/{slug}/{category}",
+     *     defaults={"category":"computers"},
+     *     requirements={"_locale": "en|fr", "category":"computers|rtv","year":"\d+"}
+     * )
+     */
+    public function index2()
+    {
+        return new Response('An advanced route example');
+    }
+
+    /**
+     * @Route({"nl": "/over-ons","en":"about-us"}, name="about_us")
+     */
+    public function index3()
+    {
+        return new Response('Translated routes');
     }
 
     /**
