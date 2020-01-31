358a60968a419f0f810c7f425183e4deed6b4bb1
Mamykin Sergei <mamykin.sergei@gmail.com>
2020-01-31 18:04:29 +0300

    Контроллеры для создания, удаления и обновления.

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
@@ -15,5 +16,34 @@ class ToDoListController extends AbstractController
         return $this->render('to_do_list/index.html.twig', [
             'controller_name' => 'ToDoListController',
         ]);
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     * @param Request $request
+     */
+    public function create(Request $request)
+    {
+//        return
+
+        exit('create hi hi hi - ' . $request->request->get('task_name'));
+    }
+
+    /**
+     * @Route("/switch_status/{id}", name="switch_status")
+     * @param int $id
+     */
+    public function switchStatus(int $id)
+    {
+        exit('switch hi hi hi - ' . $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="task_delete")
+     * @param int $id
+     */
+    public function delete(int $id)
+    {
+        exit('delete hi hi hi - ' . $id);
     }
 }
