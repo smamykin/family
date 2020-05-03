f2635c366d8b81dbad009d2ec5f7b148a2ef1e8b
Mamykin Sergei <mamykin.sergei@gmail.com>
2020-05-03 21:02:29 +0300

    login and logout

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Entity\Video;
 use App\Utils\CategoryTreeFrontPage;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class FrontController extends AbstractController
 {
@@ -94,10 +96,23 @@ class FrontController extends AbstractController
 
     /**
      * @Route("/login", name="login")
+     * @param AuthenticationUtils $helper
+     * @return Response
      */
-    public function login()
+    public function login(AuthenticationUtils $helper)
     {
-        return $this->render('front/login.html.twig');
+        return $this->render('front/login.html.twig', [
+            'error' => $helper->getLastAuthenticationError()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     * @throws Exception
+     */
+    public function logout(): void
+    {
+        throw new Exception('This should never be reached!');
     }
 
     /**
