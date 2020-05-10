d741b9d46e49a39362058b2d8555e83567531499
Mamykin Sergei <mamykin.sergei@gmail.com>
2020-05-10 18:20:30 +0300

    список видео в админке

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Video;
 use App\Form\CategoryType;
 use App\Utils\CategoryTreeAdminList;
 use App\Utils\CategoryTreeAdminOptionList;
@@ -57,7 +58,14 @@ class AdminController extends AbstractController
      */
     public function videos()
     {
-        return $this->render('admin/videos.html.twig');
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $videos = $this->getDoctrine()->getRepository(Video::class)->findAll();
+        } else {
+            $videos = $this->getUser()->getLikedVideos();
+        }
+        return $this->render('admin/videos.html.twig', [
+            'videos' => $videos,
+        ]);
     }
 
     /**
