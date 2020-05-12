ee0d90233cff1b9017ccf5a1201d8fc0da139302
Mamykin Sergei <mamykin.sergei@gmail.com>
2020-05-12 23:27:38 +0300

    обновление данных пользователя

--- src/Controller/Admin/MainController.php
+++ src/Controller/Admin/MainController.php
@@ -5,10 +5,14 @@ namespace App\Controller\Admin;
 use App\Entity\Category;
 use App\Entity\User;
 use App\Entity\Video;
+use App\Form\UserType;
 use App\Utils\CategoryTreeAdminOptionList;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/admin")
@@ -17,11 +21,40 @@ class MainController extends AbstractController
 {
     /**
      * @Route("/", name="admin_main_page")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $password_encoder
+     * @return Response
      */
-    public function index()
+    public function index(Request $request, UserPasswordEncoderInterface $password_encoder)
     {
+        /** @var User $user */
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        $is_invalid = null;
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $user->setName($request->request->get('user')['name']);
+            $user->setLastName($request->request->get('user')['last_name']);
+            $user->setEmail($request->request->get('user')['email']);
+            $password = $password_encoder->encodePassword($user, $request->request->get('user')['password']['first']);
+            $user->setPassword($password);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Your changes were saved!'
+            );
+            return $this->redirectToRoute('admin_main_page');
+        } elseif ($request->isMethod('post')) {
+            $is_invalid = 'is-invalid';
+        }
+
         return $this->render('admin/my_profile.html.twig', [
             'subscription' => $this->getUser()->getSubscription(),
+            'form' =>$form->createView(),
+            'is_invalid' => $is_invalid,
         ]);
     }
 
@@ -74,5 +107,20 @@ class MainController extends AbstractController
         $em->flush();
 
         return $this->redirectToRoute('admin_main_page');
+    }
+
+    /**
+     * @Route("delete-accaunt", name="delete_account")
+     */
+    public function deleteAccount()
+    {
+        $em = $this->getDoctrine()->getManager();
+        /** @var User $user */
+        $user = $em->getRepository(User::class)->find($this->getUser());
+
+        $em->remove($user);
+        $em->flush();
+
+        session_destroy();
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -15,20 +15,39 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = $options['data'];
         $builder
-            ->add('name', TextType::class)
-            ->add('last_name', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class
+            ->add('name', TextType::class,[
+                'empty_data'=>''
             ])
+            ->add('last_name', TextType::class,[
+                'empty_data'=>''
+            ])
+            ->add('email', EmailType::class,[
+                'empty_data'=>''
+            ])
+
+
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                // 'first_options'  => array('label' => 'Password'),
+                // 'second_options' => array('label' => 'Repeat Password'),
+            ))
         ;
+
+
+        if  ($user && in_array('ROLE_ADMIN', $user->getRoles())) {
+            $builder->add('vimeo_api_key', TextType::class, [
+                'empty_data' => ''
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'user' => null
         ]);
     }
 }
