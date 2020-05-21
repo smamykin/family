356922a35e172c8fc59f82a1d5d1b1339c68613c
Mamykin Sergei <mamykin.sergei@gmail.com>
2020-05-21 22:40:10 +0300

    output product list, get it from api

---
+++ assets/js/src/getData.js
@@ -0,0 +1,19 @@
+import { baseURL } from './config.js';
+import { show } from './view.js';
+
+let axios = require( '../libraries/axios');
+
+export const getData = () => {
+// console.log(router(event));
+    axios.get(baseURL + '/api/products')
+        .then(function (response) {
+            show(response.data['hydra:member']);
+            // console.log(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+
+}
+
